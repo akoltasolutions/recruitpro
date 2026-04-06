dd8ccfdc94e8b1e794d4666c6cc0fcd982ced971
Z User <z@container>
2026-04-06 13:00:06 +0000

    84aebea5-111e-478d-a6fe-4110b015533f

--- start-dev.sh
+++ start-dev.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Auto-restarting dev server
 cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting Next.js dev server..."
-  node node_modules/.bin/next dev -p 3000 2>&1
-  EXIT_CODE=$?
-  echo "[$(date)] Server exited with code $EXIT_CODE, restarting in 3s..."
-  sleep 3
-done
+exec bun run dev
